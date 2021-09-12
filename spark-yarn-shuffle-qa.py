0764a8f82eb861b7705305c8fbc3b3fc211a67d2
Seth Meyer <smeyer@umich.edu>
2021-09-12 14:58:50 -0400

    fix typos scala version was incorrectly added added python version

--- spark-yarn-shuffle-qa.py
+++ spark-yarn-shuffle-qa.py
@@ -1,12 +1,9 @@
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkConf
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext
 
-val conf = new SparkConf().setAppName("large rdd twitter map reduce to test spark yarn shuffle service")
-new SparkContext(conf)
+conf = SparkConf().setAppName('large spark yarn shuffle job with  twitter json data')
+sc = SparkContext(conf=conf)
 
-tf=sc.textFile("/data/twitter/decahose/json/gardenhose.2015-*")
+tf=sc.textFile("/data/twitter/decahose/json/gardenhose.2014-*")
 ct=tf.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
-ct.saveAsTextFile("2015-word-count")
-
-# should be submitted by id with access to sample twitter data
-# spark-submit --master yarn --queue arcts  --num-executors 150 --executor-memory 5g  --conf spark.shuffle.service.enabled=true --driver-memory 120g  --conf spark.driver.maxResultSize=120g --deploy-mode cluster --executor-cores 4 spark-yarn-shuffle-qa.py 
+ct.saveAsTextFile("spark-shuffle-word-count-qa-twitter-2014-json-test")
